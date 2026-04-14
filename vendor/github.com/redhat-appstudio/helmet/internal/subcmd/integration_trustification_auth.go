a707f69ab2bacc2d9a21636de302cc937caf0c6d
konflux-staging <124796549+konflux-staging[bot]@users.noreply.github.com>
2026-04-14 00:29:53 +0000

    fix(deps): update github.com/redhat-appstudio/helmet digest to 6bbd344
    Signed-off-by: konflux-staging <124796549+konflux-staging[bot]@users.noreply.github.com>

--- vendor/github.com/redhat-appstudio/helmet/internal/subcmd/integration_trustification_auth.go
+++ vendor/github.com/redhat-appstudio/helmet/internal/subcmd/integration_trustification_auth.go
@@ -11,10 +11,10 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// IntegrationTrustification is the sub-command for the "integration trustification",
-// responsible for creating and updating the Trustification integration secret.
-// It includes the coordinates to connect the cluster with remote Trustification services.
-type IntegrationTrustification struct {
+// IntegrationTrustificationAuth is the sub-command for the "integration
+// trustificationauth", responsible for creating and updating
+// the trustificationauth integration secret.
+type IntegrationTrustificationAuth struct {
 	cmd         *cobra.Command           // cobra command
 	appCtx      *api.AppContext          // application context
 	runCtx      *runcontext.RunContext   // run context (kube, logger, chartfs)
@@ -22,50 +22,51 @@ type IntegrationTrustification struct {
 	integration *integration.Integration // integration instance
 }
 
-var _ api.SubCommand = &IntegrationTrustification{}
+var _ api.SubCommand = &IntegrationTrustificationAuth{}
 
 // Cmd exposes the cobra instance.
-func (t *IntegrationTrustification) Cmd() *cobra.Command {
+func (t *IntegrationTrustificationAuth) Cmd() *cobra.Command {
 	return t.cmd
 }
 
 // Complete is a no-op in this case.
-func (t *IntegrationTrustification) Complete(_ []string) error {
+func (t *IntegrationTrustificationAuth) Complete(_ []string) error {
 	var err error
 	t.cfg, err = bootstrapConfig(t.cmd.Context(), t.appCtx, t.runCtx)
 	return err
 }
 
 // Validate checks if the required configuration is set.
-func (t *IntegrationTrustification) Validate() error {
+func (t *IntegrationTrustificationAuth) Validate() error {
 	return t.integration.Validate()
 }
 
-// Run creates or updates the Trustification integration secret.
-func (t *IntegrationTrustification) Run() error {
+// Run creates or updates the trustificationauth integration secret.
+func (t *IntegrationTrustificationAuth) Run() error {
 	return t.integration.Create(t.cmd.Context(), t.runCtx, t.cfg)
 }
 
-// NewIntegrationTrustification creates the sub-command for the "integration
-// trustification" responsible to manage the integrations with the
+// NewIntegrationTrustificationAuth creates the sub-command for the "integration
+// trustificationauth" responsible to manage the authentication integrations with the
 // Trustification service.
-func NewIntegrationTrustification(
+func NewIntegrationTrustificationAuth(
 	appCtx *api.AppContext,
 	runCtx *runcontext.RunContext,
 	i *integration.Integration,
-) *IntegrationTrustification {
-	t := &IntegrationTrustification{
+) *IntegrationTrustificationAuth {
+	t := &IntegrationTrustificationAuth{
 		cmd: &cobra.Command{
-			Use: "trustification [flags]",
+			Aliases: []string{"trustification-auth"},
+			Use:     "trustification-auth [flags]",
 			Short: fmt.Sprintf(
-				"Integrates a Trustification instance into %s",
+				"Integrates a trustification-auth instance into %s",
 				appCtx.Name,
 			),
 			Long: fmt.Sprintf(`
-Manages the Trustification integration with %s by storing the information
+Manages the trustification-auth integration with %s by storing the credentials
 required by %s services to interact with Trustification.
 
-The information is stored in a Kubernetes Secret in the namespace
+The credentials are stored in a Kubernetes Secret in the namespace
 configured for %s.`,
 				appCtx.Name,
 				appCtx.Name,
